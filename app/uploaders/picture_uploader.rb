410b6ea38e34da89b3cf17e70d8141af81cac3d6
Arpit Agarwal <agarwalarpit22@gmail.com>
2016-10-31 19:17:42 +0000

    Image upload complete

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,10 +1,11 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
+  
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -25,7 +26,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process :scale => [200, 300]
+  process :resize_to_limit => [300, 300]
   #
   # def scale(width, height)
   #   # do something
